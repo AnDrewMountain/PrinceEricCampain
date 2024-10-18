2c0381c181b252284a6157bd82bfc6d400fef3bc
AnDrewMountain <andrewjmountain@gmail.com>
2024-10-17 22:35:12 -0500

    adding various scetions to the html

---
+++ app.js
@@ -0,0 +1,8 @@
+// app.js
+document.querySelector("button").addEventListener("click", function() {
+    // Display the alert message
+    alert("Thank you for donating your voice!");
+    
+    // After the alert, hide the page's content
+    document.body.style.display = 'none';
+});
